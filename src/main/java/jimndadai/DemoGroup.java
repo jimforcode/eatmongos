ae5ad27f8fe4a3373a4ae254160299bd7fca99d3
zj <zj@xiaoxiao.la>
2015-07-18 16:04:48 +0800

    ddddddddd

--- src/main/java/jimndadai/DemoGroup.java
+++ src/main/java/jimndadai/DemoGroup.java
@@ -9,11 +9,20 @@ import com.mongodb.Mongo;
 
 import demo.pojo.GroupPojo;
 
+/*
+ *
+ @see http://docs.spring.io/spring-data/data-mongo/docs/1.8.0.M1/reference/html/#mongo.group
+ * 
+ */
 public class DemoGroup {
 	public static void main(String[] args) throws UnknownHostException {
 		MongoOperations template = new MongoTemplate(new Mongo(), "database");
-		template.insert(new GroupPojo("f","f","f",1));
-		
+		template.insert(new GroupPojo("f1", "f2", "f3", 1));
+		template.insert(new GroupPojo("f1", "f22", "f33", 1));
+		template.insert(new GroupPojo("f1", "f22", "f3", 1));
+		template.insert(new GroupPojo("f1", "f2", "f3", 1));
+		template.insert(new GroupPojo("f1", "f2", "f3", 1));
+
 		template.dropCollection("groupPojo");
 	}
 }
